e73bedae45d010962e40a9274dfe78f43820ec36
Leonardo Mattos <ldmattos@inf.ufsm.br>
2023-09-04 22:25:05 -0300

    Falta refatorar botDecisions (atualmente arquivo auxiliar com funções funcionais que serão integradas), bot sabe colocar bandeira e escolher células vázias (quando possível realizar dedução direta)

--- Campo minado/game.cpp
+++ Campo minado/game.cpp
@@ -68,64 +68,6 @@ void dispose(SDL_Renderer** renderer, SDL_Window** window)
 	SDL_Quit();
 }
 
-static int countClosedCells(Board** board, int size, int i, int j)
-{
-	int closedCount = 0, newRow, newCol;
-	for (int linha = -1; linha < 2; linha++)
-	{
-		for (int coluna = -1; coluna < 2; coluna++)
-		{
-			newRow = i + linha;
-			newCol = j + coluna;
-			if (newRow >= 0 && newRow < size && newCol >= 0 && newCol < size && board[newRow][newCol].isOpen == 0)
-			{
-				if(board[newRow][newCol].isFlag == 0)
-					closedCount++;
-			}
-		}
-	}
-
-	return closedCount;
-}
-
-static int searchFlag(Game* game, Board** board, int i, int j)
-{
-	int newRow, newCol;
-	int qtdFlags = 0;
-	for (int linha = -1; linha < 2; linha++)
-	{
-		for (int coluna = -1; coluna < 2; coluna++)
-		{
-			newRow = i + linha;
-			newCol = j + coluna;
-			if (newRow >= 0 && newRow < game->size && newCol >= 0 && newCol < game->size && board[newRow][newCol].isFlag != 0)
-			{
-				qtdFlags++;
-			}
-		}
-	}
-	std::cout << i << " " << j << " " << qtdFlags << std::endl;
-	return qtdFlags;
-}
-
-static void searchFreeCell(Game* game, Board** board, int i, int j)
-{
-	int newRow, newCol;
-	for (int linha = -1; linha < 2; linha++)
-	{
-		for (int coluna = -1; coluna < 2; coluna++)
-		{
-			newRow = i + linha;
-			newCol = j + coluna;
-
-			if (newRow >= 0 && newRow < game->size && newCol >= 0 && newCol < game->size && board[newRow][newCol].isOpen == 0)
-			{
-				board[newRow][newCol].isOpen = 1;
-			}
-		}
-	}
-}
-
 static void playBot(Game* game, Board** board)
 {
 	for (int i = 0; i < game->size; i++)
